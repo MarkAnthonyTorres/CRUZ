f26503e656195457c8feb0938953160d2c1580c4
MarkAnthonyTorres <markanthonyjoseph.torres@gsfe.tupcavite.edu.ph>
2022-01-08 17:30:21 +0800

    hwk

--- logcommon.js
+++ logcommon.js
@@ -1,7 +1,25 @@
-var LogName = [];
-var LogID = [];
+var LAbtn = document.getElementById("Loginsub");
+LAbtn.addEventListener("click",displayinputs);
 
-LogName.forEach(function(item){
-    console.log(item)
-});
+var row = 1;
 
+function displayinputs(){
+    var GetE = document.getElementById("Email");
+    var GetP = document.getElementById("Password");
+
+    var display = document.getElementById("Loglist");
+
+    var newrow = display.insertRow(row);
+
+    var cell1 = newrow.insertCell(0);
+    var cell2 = newrow.insertCell(1);
+    
+
+    cell1.innerHTML = GetE.value;
+    cell2.innerHTML = GetP.value;
+
+    console.log(GetE.value);
+    console.log(GetP.value);
+
+    row++;
+}
